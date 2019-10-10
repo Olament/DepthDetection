db22ccf9a4ec8746f15155795491e4a77d8af668
Zixuan Guo <i@zxguo.me>
2019-10-10 01:33:37 -0500

    Initial Commit

---
+++ DepthDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DepthDetection
+//
+//  Created by Zixuan on 10/10/19.
+//  Copyright © 2019 Zixuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
