97ce78ef17b5f81c594b4fb10a7e3bae18df5669
Zixuan Guo <i@zxguo.me>
2019-10-13 22:43:57 -0500

    init commit

---
+++ DepthDetection/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  DepthDetection
+//
+//  Created by Zixuan on 10/10/19.
+//  Copyright © 2019 Zixuan. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
